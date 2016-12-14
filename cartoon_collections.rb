17e67c04631fbff58ee75b8bff4cad2f9c9556b5
Abdalkhalek ahmed <eng.abdalkalk@gmail.com>
2016-12-14 17:51:27 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves=["Dopey", "Grumpy", "Bashful"])# code an argument here
   # Your code here
+cou=0
+while cou < dwarves.size
+  puts "#{cou+1} #{dwarves[cou]}"
+  cou=cou+1
+end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls=["earth", "wind", "fire", "water", "heart"])# code an argument here
   # Your code here
+    planeteer_calls.map { |call| call.capitalize + "!" }
+  end
+
+
+def long_planeteer_calls(calls)# code an argument here
+  # Your code here
+  calls.select do |item|
+     if item.length > 4
+        return true
+      else
+         return false
+       end
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
 
-def find_the_cheese# code an argument here
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
+#cou=0
+#if cheese_types[cou]="cheddar" || cheese_types[cou]="gouda" || cheese_types[cou]="camembert"
+#cheese_types[cou]
+#else
+#return nil
+#cou=cou=1
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+#end
